452eaa9e2cf1483641f470253c5e9c20e0303434
Thorexus <watcharaphan@artisan.co.th>
2020-11-24 14:20:39 +0700

    add test

--- api/src/bin/www.js
+++ api/src/bin/www.js
@@ -2,11 +2,9 @@
 /**
  * Module dependencies.
  */
-// import debug from "debug";
-// import http from "http";
-// import app from "../app";
-var app = require("../app");
-var http = require("http");
+import debug from 'debug';
+import http from 'http';
+import app from '../app';
 /**
  * Normalize a port into a number, string, or false.
  */
@@ -26,13 +24,51 @@ const normalizePort = (val) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      alert(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      alert(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+};
 
 /**
  * Create HTTP server.
  */
 const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
 server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
 // next code block goes here
